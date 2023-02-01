c2a1c9f494f02b6f93a0d1fd88bed4cc42805b72
Brenton Andrews <brenton.j.andrews@gmail.com>
2023-02-01 11:24:02 -0700

    Feed share input skeleton completed / styled.

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,10 +1,42 @@
+import { AddAPhoto, Label, LocationOn } from "@mui/icons-material";
 import "./feed.css";
 
 const Feed = () => {
     return (
         <div className="feed">
+
             <div className="feedWrapper">
-                Hello from the feed component!
+
+                <div className="feedShare">
+                    <div className="feedShareWrapper">
+
+                        <div className="shareTop">
+                            <img className="shareInputUserImage" src="/assets/defaultProfileImage.png" alt=""/>
+                            <input className="shareInput" type="text" placeholder="What's on your mind?"/>
+                        </div>
+
+                        <hr className="shareHr" />
+
+                        <div className="shareBottom">
+                            <div className="shareOption">
+                                <AddAPhoto />
+                                <span className="shareOptionText"> Add a Photo </span>
+                            </div>
+
+                            <div className="shareOption">
+                                <Label />
+                                <span className="shareOptionText"> Tag Friends </span>
+                            </div>
+
+                            <div className="shareOption">
+                                <LocationOn />
+                                <span className="shareOptionText"> Add a Location </span>
+                            </div>
+
+                            <button className="shareButton"> Share </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
