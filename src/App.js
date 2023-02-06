f16040e0ca03b0efcfefe3814d0af34484acb0be
Brenton Andrews <brenton.j.andrews@gmail.com>
2023-02-05 19:27:17 -0700

    Routing added on app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 // Import order: libraries and modules -> components -> assets.  Missing anything?
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
@@ -10,7 +16,34 @@ import "./app.css";
 function App() {
   return (
     <div className="App">
-      <Login />
+      <BrowserRouter>
+        <Routes>
+
+          <Route 
+            exact path="/"
+            element={ <Home /> }
+          />
+
+          <Route 
+            exact path="/profile"
+            element={ <Profile /> }
+          />
+
+
+          <Route 
+            exact path="/login"
+            element={ <Login /> }
+          />
+
+
+          <Route 
+            exact path="/register"
+            element={ <Register /> }
+          />
+
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
